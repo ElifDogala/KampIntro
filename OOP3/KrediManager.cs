56b616c34f4cda1c4b5355797df5168bf1c09ec7
ElifDogala <elif.dogala@hotmail.com>
2022-06-01 18:59:40 +0300

    c# 5.kamp

---
+++ OOP3/KrediManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+   interface IKrediManager  //interfaceleri I harfiyle başlatırız
+    {
+        void Hesapla();
+        void BiseyYap();
+
+        
+    }
+}
